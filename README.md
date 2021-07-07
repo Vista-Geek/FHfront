a126f47f1bb7d0ef9711e6b0e272b148e2bb364e
Santigp258 <santigp258@gmail.com>
2021-07-07 01:56:18 -0500

    Theme colors

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 ### Fantasy House Frontend
+
+## Tailwind Notes
+
+_Theme colors have the suffix "Cust", this is very important for no rewrite default colors that Tailwind provided._
+
+### Example
+
+```bash
+blueCust: "var(--blue)"
+```
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
+const colors = require("tailwindcss/colors");
 module.exports = {
   darkMode: 'class', // or 'media' or 'class'
   purge: ["./src/**/*.tsx", "./public/index.html"],
   mode: "jit",
   theme: {
+    colors: {
+      ...colors,
+      blueCust: "var(--blue)",
+      redCust: "var(--red)",
+      lightblueCust: "var(--lightblue)",
+      blackCust: "var(--dark)",
+      white: "var(--light)",
+    },
     screens: {
       sm: '640px',
       md: '768px',
