e75b8c1c93f6e0a5664ee7ef9a422064b92a3b16
Santigp258 <santigp258@gmail.com>
2021-07-08 16:23:54 -0500

    border-card

--- src/modules/login/Card.tsx
+++ src/modules/login/Card.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
-
 const Card = () => {
-  return <div className="bg-purpleCust-dark w-full min-h-380 mt-5">hola</div>;
+  return <div className="bg-purpleCust-dark w-full min-h-380 mt-5 border-card">hola</div>;
 };
 
 export default Card;
