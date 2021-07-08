7c5eb3b71887851cae43a09f7d0c0584a34197ac
Douglas Lovera <doulovera@gmail.com>
2021-07-08 16:26:29 -0500

    Changes in /landing

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,24 @@
-const colors = require("tailwindcss/colors");
+const colors = require('tailwindcss/colors');
 module.exports = {
   darkMode: 'class', // or 'media' or 'class'
-  purge: ["./src/**/*.tsx", "./public/index.html"],
-  mode: "jit",
+  purge: ['./src/**/*.tsx', './public/index.html'],
+  mode: 'jit',
   theme: {
+    borderRadius: {
+      fh: '0px 25px 0px 15px'
+    },
     colors: {
       ...colors,
       purpleCust: {
-        DEFAULT: "var(--purple)",
-        bg: "var(--purple-bg)",
-        dark: "var(--purple-dark)",
-        light: "var(--purple-light)",
-        card: "var(--purple-card)",
-        text: "var(--purple-text)",
+        DEFAULT: 'var(--purple)',
+        bg: 'var(--purple-bg)',
+        dark: 'var(--purple-dark)',
+        light: 'var(--purple-light)',
+        card: 'var(--purple-card)',
+        text: 'var(--purple-text)'
       },
-      orangeCust: "var(--orange)",
-      white: "var(--light)",
+      orangeCust: 'var(--orange)',
+      white: 'var(--light)'
     },
     screens: {
       sm: '640px',
@@ -25,19 +28,19 @@ module.exports = {
       '2xl': '1536px'
     },
     fontFamily: {
-      'display': ['Oswald'],
+      display: ['Oswald']
     },
     minHeight: {
-      "380": "380px"
+      380: '380px'
     },
     extend: {
       backgroundImage: theme => ({
-        'hero': "url('/images/hero.jpg')",
+        hero: "url('/images/hero.jpg')"
       })
-    },
+    }
   },
   variants: {
-    extend: {},
+    extend: {}
   },
-  plugins: [],
-}
+  plugins: []
+};
