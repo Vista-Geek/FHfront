cbfe3f1689487a9192a047744292139f7b0fa7b3
Santigp258 <santigp258@gmail.com>
2021-07-18 14:06:33 -0500

    discord-banner

--- src/modules/Details/index.tsx
+++ src/modules/Details/index.tsx
@@ -1,44 +1,20 @@
-// import DiscordBanner from "@components/Banner.tsx/DiscordBanner";
 import React from "react";
+import DiscordBanner from "@components/Banner/DiscordBanner";
 import GlobalNavbar from "@components/Navbar/GlobalNavbar";
-import ImageGallery from "react-image-gallery";
-import { IoMdHeart } from "react-icons/io";
+import Head from "next/head";
+import DetailsCard from "./DetailsCard";
 
 const Details = () => {
-  const images = [
-    {
-      original: "https://picsum.photos/id/1018/1000/600/",
-      thumbnail: "https://picsum.photos/id/1018/250/150/",
-    },
-    {
-      original: "https://picsum.photos/id/1015/1000/600/",
-      thumbnail: "https://picsum.photos/id/1015/250/150/",
-    },
-    {
-      original: "https://picsum.photos/id/1019/1000/600/",
-      thumbnail: "https://picsum.photos/id/1019/250/150/",
-    },
-  ];
   return (
     <>
+      <Head>
+        <title>Fantasy House - House Details </title>
+      </Head>
       <GlobalNavbar />
-      <div className="pt-24 px-4">
-        <div className="flex justify-between mb-7">
-          <h2 className="text-xl">House Details</h2>
-          <span className="text-xl text-purpleCust-text">
-            Add Favorite <IoMdHeart className="text-3xl text-red-600 inline" />
-          </span>
-        </div>
-        <div className="w-full">
-          <ImageGallery
-            items={images}
-            showPlayButton={false}
-            showNav={false}
-            thumbnailPosition="bottom"
-          />
-        </div>
+      <div className="md:pt-24 pt-20 mb-5">
+        <DetailsCard />
       </div>
-      {/* <DiscordBanner /> */}
+      <DiscordBanner />
     </>
   );
 };
