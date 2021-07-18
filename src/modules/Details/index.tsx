362fc048e2eed2097c0682a67c0aa0486a828d4e
Santigp258 <santigp258@gmail.com>
2021-07-18 14:24:53 -0500

    discord banner and layout component

--- src/modules/Details/index.tsx
+++ src/modules/Details/index.tsx
@@ -3,6 +3,7 @@ import DiscordBanner from "@components/Banner/DiscordBanner";
 import GlobalNavbar from "@components/Navbar/GlobalNavbar";
 import Head from "next/head";
 import DetailsCard from "./DetailsCard";
+import Layout from "@components/Layout/Layout";
 
 const Details = () => {
   return (
@@ -10,11 +11,12 @@ const Details = () => {
       <Head>
         <title>Fantasy House - House Details </title>
       </Head>
-      <GlobalNavbar />
-      <div className="md:pt-24 pt-20 mb-5">
-        <DetailsCard />
-      </div>
-      <DiscordBanner />
+      <Layout>
+        <div className="md:pt-24 pt-20 mb-5">
+          <DetailsCard />
+        </div>
+        <DiscordBanner />
+      </Layout>
     </>
   );
 };
--- src/modules/components/Layout/Layout.tsx
+++ src/modules/components/Layout/Layout.tsx
@@ -1,19 +1,13 @@
 import React from "react";
-import Navbar from "@components/Navbar/Navbar";
-import Button from "@components/Buttons/Button";
+import GlobalNavbar from "@components/Navbar/GlobalNavbar";
+import Footer from "@components/Footer";
 
 const Layout: React.FC = ({ children }) => {
   return (
     <>
-      <Navbar>
-        <Button
-          type="button"
-          width="w-full"
-          classnames="bg-purpleCust-light w-24 font-herbo"
-          title="Sign up"
-        />
-      </Navbar>
+      <GlobalNavbar />
       {children}
+      <Footer />
     </>
   );
 };
