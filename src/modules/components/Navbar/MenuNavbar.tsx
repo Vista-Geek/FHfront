fc65e6584ec537a8f5444b8ae8f002b6b12f72dd
Douglas Lovera <doulovera@gmail.com>
2021-07-19 15:47:52 -0500

    feat: Navbar for mobile is done

--- src/modules/components/Navbar/MenuNavbar.tsx
+++ src/modules/components/Navbar/MenuNavbar.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Link from "next/link";
 import Image from "next/image";
 import ButtonNavbar from "@components/Buttons/ButtonNavbar";
@@ -6,40 +6,97 @@ import ProfileDropdown from "@components/DropDowns/ProfileDropdown";
 import { IoMdHeart } from "react-icons/io";
 
 const MenuNavbar = () => {
+  const [isVisible, setIsVisible] = useState(false);
+  const toggleNav = () => {
+    setIsVisible(!isVisible);
+  };
+  useEffect(() => {
+    /*
+      Cambia el overflow del <body> según si el
+      Navbar está abierto, para que no haya scroll.
+    */
+    document.body.classList.toggle("overflow-hidden", isVisible);
+  }, [isVisible]);
+
+  /*
+    Si el tamaño de la pantalla incrementa y el
+    isVisible es true, esto lo setea a false.
+  */
+  const handleResize = () => {
+    if (window.innerWidth > 640) {
+      setIsVisible(false);
+    }
+  };
+  useEffect(() => window.addEventListener("resize", handleResize));
+
   return (
-    <div className="flex justify-center items-center">
-      <ButtonNavbar
-        href="/"
-        title="Buy"
-        styles="bg-opacity-0 border-b-2 border-purpleCust rounded-none mr-1"
-      />
-      <ButtonNavbar
-        href="/"
-        title="Sell"
-        styles="bg-opacity-0  border-purpleCust rounded-none"
-      />
-      <ProfileDropdown />
-      <div className="mx-6 mr-8 hidden sm:block">
-        <Link href="/favorites">
-          <a>
-            <IoMdHeart className="text-3xl" />
-          </a>
-        </Link>
+    <>
+      <div className="hidden sm:flex justify-center items-center">
+        <ButtonNavbar
+          href="/"
+          title="Buy"
+          styles="bg-opacity-0 border-b-2 border-purpleCust rounded-none mr-1"
+        />
+        <ButtonNavbar
+          href="/"
+          title="Sell"
+          styles="bg-opacity-0  border-purpleCust rounded-none"
+        />
+        <ProfileDropdown />
+        <div className="mx-6 mr-8 hidden sm:block">
+          <Link href="/favorites">
+            <a>
+              <IoMdHeart className="text-3xl" />
+            </a>
+          </Link>
+        </div>
+        <div className="hidden sm:block">
+          <Link href="/profile">
+            <a>
+              <Image
+                src="/images/user.png"
+                alt="logo"
+                width={50}
+                height={50}
+                className="cursor-pointer"
+              />
+            </a>
+          </Link>
+        </div>
       </div>
-      <div className=" hidden sm:block">
-        <Link href="/profile">
-          <a>
-            <Image
-              src="/images/user.png"
-              alt="logo"
-              width={50}
-              height={50}
-              className="cursor-pointer"
-            />
-          </a>
-        </Link>
+      <div className="sm:hidden">
+        <button onClick={toggleNav}>
+          <i className="fas fa-bars text-2xl pt-1 px-2"></i>
+        </button>
+        <ul
+          className={
+            "text-lg font-herbo text-center absolute top-0 left-0 mt-16 w-screen h-screen bg-purpleCust-dark " +
+            (isVisible ? "flex flex-col overflow-hidden" : "hidden")
+          }
+        >
+          <li className="hover:text-purpleCust-text hover:font-semibold pt-8">
+            <Link href="/home">
+              <a>Buy House</a>
+            </Link>
+          </li>
+          <li className="hover:text-purpleCust-text hover:font-semibold pt-8">
+            <Link href="/sell">
+              <a>Sell House</a>
+            </Link>
+          </li>
+          <li className="hover:text-purpleCust-text hover:font-semibold pt-8">
+            <Link href="/favorites">
+              <a>Favorites</a>
+            </Link>
+          </li>
+          <li className="hover:text-purpleCust-text hover:font-semibold pt-8">
+            <Link href="/profile">
+              <a>Profile</a>
+            </Link>
+          </li>
+        </ul>
       </div>
-    </div>
+    </>
   );
 };
 
