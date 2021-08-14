4ac52a79fe505c12193106d34f8ece862c60a0d1
Santigp258 <santigp258@gmail.com>
2021-08-13 19:39:17 -0500

    loader error

--- src/guard/AuthGuard.tsx
+++ src/guard/AuthGuard.tsx
@@ -18,13 +18,15 @@ export const AuthGuard: React.FC = ({ children }) => {
   }, [checking, router, userData]);
 
   /* show loading indicator while the auth provider is still isLoading */
-  if (checking || !userData) {
+  if (checking) {
     return (
-      <div className="flex overflow-hidden fixed top-0 right-0 bottom-0 left-0 z-50 flex-col justify-center items-center w-full h-screen bg-opacity-80 bg-gray-300">
-        <div className="mb-4 w-12 h-12 rounded-full border-4 border-t-4 border-gray-200 ease-linear loader animate-spinner" />
-        <h2 className="text-xl font-semibold text-center text-primary-base">
-          Loading...
-        </h2>
+      <div className="absolute h-screen w-screen z-50">
+        <div className="flex overflow-hidden fixed top-0 right-0 bottom-0 left-0 z-50 flex-col justify-center items-center w-full h-screen bg-opacity-80 bg-gray-300">
+          <div className="mb-4 w-12 h-12 rounded-full border-4 border-t-4 border-gray-200 ease-linear loader animate-spinner" />
+          <h2 className="text-xl font-semibold text-center text-primary-base">
+            Loading...
+          </h2>
+        </div>
       </div>
     );
   }
